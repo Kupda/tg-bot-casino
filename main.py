baccaa8ac58b1e7a451a3a599d9d4dd7461a7d78
kupda <kupda27@gmail.com>
2025-03-28 22:15:31 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import os
 from dotenv import load_dotenv
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
+
+# Считываем токен из файла .env
 load_dotenv()
-MY_ENV_VAR = os.getenv('TOKEN')
-print(MY_ENV_VAR)
+TOKEN = os.getenv('TOKEN')
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(TOKEN)
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
