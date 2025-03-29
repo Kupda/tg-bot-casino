ed0011b20689e5b16c4f9ee46d3fce00357c82c2
kupda <kupda27@gmail.com>
2025-03-29 08:47:34 +0300

    update main.py (add simple casino)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from random import randint
 
 
 # Считываем токен из файла .env
@@ -20,7 +21,23 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer(f"""Привет, {message.from_user.full_name}, крутанем?\n
+/roll - чтобы крутануть
+/stats - чтобы узнать свою статистику""")
+
+# Хэндлер на команду /roll
+@dp.message(Command("roll"))
+async def cmd_start(message: types.Message):
+    slot1 = randint(1, 6)
+    slot2 = randint(1, 6)
+    slot3 = randint(1, 6)
+    await message.answer(f"""Выпало: {slot1}|{slot2}|{slot3}""")
+    if slot1 == slot2 and slot2 == slot3:
+        await message.answer(f"""Поздравляю! Вы выиграли 5 монет!""")
+    elif slot1 == slot2 and slot2 != slot3 or slot1 == slot3 and slot1 != slot2 or slot2 == slot3 and slot2 != slot1:
+        await message.answer(f"""Поздравляю! Вы выиграли 3 монеты!""")
+    else:
+        await message.answer(f"""К сожалению, вы ничего не выиграли =(""")
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
